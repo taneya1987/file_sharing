3922b96d98e305527d39dabc0e8c05d22b200843
TANEYA SHARMA <taneya-pavitra@192-168-1-113.tpgi.com.au>
2022-11-01 17:00:28 +1100

    Recommiting the handler changes day 1

--- backend/share-handler.js
+++ backend/share-handler.js
@@ -3,45 +3,44 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { randomUUID } from 'node:crypto'
 
 const { BUCKET_NAME } = process.env
-const EXPIRE_DEFAULT = 24 * 60 * 60
+const EXPIRY_DEFAULT = 24 * 60 * 60
 
 const s3Client = new S3Client()
 
 export const handleEvent = async (event, context) => {
   // Create a key (file name)
   const id = randomUUID()
-  const key = `shares/${id[1]}/${id}`
-  
+  const key = `shares/${id[0]}/${id[1]}/${id}`
 
-   // Create the download URL
+  // Create the download URL
   const getCommand = new GetObjectCommand({
     Bucket: BUCKET_NAME,
     Key: key
   })
   const retrievalUrl = await getSignedUrl(
     s3Client, getCommand, {
-      expiresIn:  EXPIRE_DEFAULT
+      expiresIn: EXPIRY_DEFAULT
     }
   )
 
-  // Create the upload URL
+  // Create an upload URL
   const putCommand = new PutObjectCommand({
     Bucket: BUCKET_NAME,
     Key: key
   })
+
   const uploadUrl = await getSignedUrl(
     s3Client, putCommand, {
-      expiresIn:  EXPIRE_DEFAULT
+      expiresIn: EXPIRY_DEFAULT
     }
-  )  
+  )
 
   return {
-    statusCode : 201,
+    statusCode: 201,
     body: `
-    Upload with: curl -X PUT -T <filename> ${uploadUrl}
-    Download with: curl ${retrievalUrl}
+      Upload with: curl -X PUT -T <filename> ${uploadUrl} 
+
+      Download with: curl ${retrievalUrl}
     `
   }
-
 }
-
